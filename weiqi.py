245cdef2991acc44f159bf6aff0f8d04c58dd7f9
satoko5793 <50042290+satoko5793@users.noreply.github.com>
2022-11-15 12:58:49 +0800

    前后端类分离

--- weiqi.py
+++ weiqi.py
@@ -5,34 +5,18 @@ from tkinter.ttk import *
 import copy
 import tkinter.messagebox
 
-
-class Weiqi(Tk):
-    def __init__(self, chessboard_size=9):
+class Window(Tk):
+    def __init__(self, backend, window_size, dd, p, chessboard_size=9):
         Tk.__init__(self)
+        self.backend = backend
         # 模式，九路棋：9，十三路棋：13，十九路棋：19
         self.mode_num = chessboard_size
-        self.window_size = 1.8
-        self.dd = 360 * self.window_size / (self.mode_num - 1)  # 棋盘每格的边长
+        self.window_size = window_size
+        self.dd = dd  # 棋盘每格的边长
         # 棋盘的相对矫正比例
-        self.p = 1 if self.mode_num == 9 else (2 / 3 if self.mode_num == 13 else 4 / 9)
-        # 棋盘阵列,超过边界：-1，无子：0，黑棋：1，白棋：2
-        self.chessboard = [[0 for i in range(self.mode_num + 2)] for i in range(self.mode_num + 2)]
-        for m in range(self.mode_num + 2):
-            for n in range(self.mode_num + 2):
-                if m == 0 or n == 0 or m == self.mode_num + 1 or n == self.mode_num + 1:
-                    self.chessboard[m][n] = -1
-        # 拷贝三份棋盘“快照”，悔棋和判断“打劫”时需要作参考
-        self.last_3_chessboard = copy.deepcopy(self.chessboard)
-        self.last_2_chessboard = copy.deepcopy(self.chessboard)
-        self.last_1_chessboard = copy.deepcopy(self.chessboard)
+        self.p = p
         # 记录鼠标经过的地方，用于显示shadow时
         self.cross_last = None
-        # 当前轮到的玩家，黑：0，白：1，执黑先行
-        self.present = 0
-        # 初始停止，点击“开始游戏”运行游戏
-        self.stop = True
-        # 悔棋次数，次数大于0才可悔棋，初始置0（初始不能悔棋），悔棋后置0，下棋或弃手时恢复为1，以禁止连续悔棋
-        self.regretchance = 0
         # 图片资源，存放在当前目录下的/Pictures/中
         self.photoW = PhotoImage(file="./Pictures/W.png")
         self.photoB = PhotoImage(file="./Pictures/B.png")
@@ -49,19 +33,19 @@ class Weiqi(Tk):
         self.canvas_bottom = Canvas(self, bg='#369', bd=0, width=600 * self.window_size, height=400 * self.window_size)
         self.canvas_bottom.place(x=0, y=0)
         # 几个功能按钮
-        self.startButton = Button(self, text='开始游戏', command=self.start)
+        self.startButton = Button(self, text='开始游戏', command=self.backend.start)
         self.startButton.place(x=480 * self.window_size, y=200 * self.window_size)
-        self.passmeButton = Button(self, text='弃一手', command=self.passme)
+        self.passmeButton = Button(self, text='弃一手', command=self.backend.passme)
         self.passmeButton.place(x=480 * self.window_size, y=225 * self.window_size)
-        self.regretButton = Button(self, text='悔棋', command=self.regret)
+        self.regretButton = Button(self, text='悔棋', command=self.backend.regret)
         self.regretButton.place(x=480 * self.window_size, y=250 * self.window_size)
         # 初始悔棋按钮禁用
         self.regretButton['state'] = DISABLED
-        self.replayButton = Button(self, text='重新开始', command=self.reload)
+        self.replayButton = Button(self, text='重新开始', command=self.backend.reload)
         self.replayButton.place(x=480 * self.window_size, y=275 * self.window_size)
-        self.newGameButton1 = Button(self, text=('十三' if self.mode_num == 9 else '九') + '路棋', command=self.newGame1)
+        self.newGameButton1 = Button(self, text=('十三' if self.mode_num == 9 else '九') + '路棋', command=self.backend.newGame1)
         self.newGameButton1.place(x=480 * self.window_size, y=300 * self.window_size)
-        self.newGameButton2 = Button(self, text=('十三' if self.mode_num == 19 else '十九') + '路棋', command=self.newGame2)
+        self.newGameButton2 = Button(self, text=('十三' if self.mode_num == 19 else '十九') + '路棋', command=self.backend.newGame2)
         self.newGameButton2.place(x=480 * self.window_size, y=325 * self.window_size)
         self.quitButton = Button(self, text='退出游戏', command=self.quit)
         self.quitButton.place(x=480 * self.window_size, y=350 * self.window_size)
@@ -100,17 +84,19 @@ class Weiqi(Tk):
         # 鼠标移动时，调用shadow函数，显示随鼠标移动的棋子
         self.canvas_bottom.bind('<Motion>', self.shadow)
         # 鼠标左键单击时，调用getdown函数，放下棋子
-        self.canvas_bottom.bind('<Button-1>', self.getDown)
+        self.canvas_bottom.bind('<Button-1>', self.backend.getDown)
         # 设置退出快捷键<Ctrl>+<D>，快速退出游戏
-        self.bind('<Control-KeyPress-d>', self.keyboardQuit)
+        self.bind('<Control-KeyPress-d>', self.backend.keyboardQuit)
 
     # 以下四个函数实现了右侧太极图的动态创建与删除
     def create_pW(self):
-        self.pW = self.canvas_bottom.create_image(500 * self.window_size + 11, 65 * self.window_size, image=self.photoW)
+        self.pW = self.canvas_bottom.create_image(500 * self.window_size + 11, 65 * self.window_size,
+                                                  image=self.photoW)
         self.canvas_bottom.addtag_withtag('image', self.pW)
 
     def create_pB(self):
-        self.pB = self.canvas_bottom.create_image(500 * self.window_size - 11, 65 * self.window_size, image=self.photoB)
+        self.pB = self.canvas_bottom.create_image(500 * self.window_size - 11, 65 * self.window_size,
+                                                  image=self.photoB)
         self.canvas_bottom.addtag_withtag('image', self.pB)
 
     def del_pW(self):
@@ -119,18 +105,104 @@ class Weiqi(Tk):
     def del_pB(self):
         self.canvas_bottom.delete(self.pB)
 
-    # 开始游戏函数
-    def start(self):
+    def start(self, present):
         # 删除右侧太极图
         self.canvas_bottom.delete(self.pW)
         self.canvas_bottom.delete(self.pB)
         # 利用右侧图案提示开始时谁先落子
-        if self.present == 0:
+        if present == 0:
             self.create_pB()
             self.del_pW()
         else:
             self.create_pW()
             self.del_pB()
+
+    def player_change(self, present):
+        if present == 0:
+            self.create_pW()
+            self.del_pB()
+        else:
+            self.create_pB()
+            self.del_pW()
+
+    def regret(self, present):
+        self.canvas_bottom.delete('image')
+        if present == 0:
+            self.create_pB()
+        else:
+            self.create_pW()
+
+    def reload(self):
+        self.canvas_bottom.delete('image')
+        self.create_pB()
+
+    # 显示鼠标移动下棋子的移动
+    def shadow(self, event):
+        if not self.backend.stop:
+            # 找到最近格点，在当前位置靠近的格点出显示棋子图片，并删除上一位置的棋子图片
+            if (20 * self.window_size < event.x < 380 * self.window_size) \
+                    and (20 * self.window_size < event.y < 380 * self.window_size):
+                dx = (event.x - 20 * self.window_size) % self.dd
+                dy = (event.y - 20 * self.window_size) % self.dd
+                self.cross = self.canvas_bottom.create_image(
+                    event.x - dx + round(dx / self.dd) * self.dd + 22 * self.p,
+                    event.y - dy + round(dy / self.dd) * self.dd - 27 * self.p,
+                    image=self.photoWBU_list[self.backend.present])
+                self.canvas_bottom.addtag_withtag('image', self.cross)
+                if self.cross_last != None:
+                    self.canvas_bottom.delete(self.cross_last)
+                self.cross_last = self.cross
+
+    # 警告消息框，接受标题和警告信息
+    def showwarningbox(self, title, message):
+        self.canvas_bottom.delete(self.cross)
+        tkinter.messagebox.showwarning(title, message)
+
+    def add_image(self, x, y, p1, p2, present):
+        self.image_added = self.canvas_bottom.create_image(x, y, image=self.photoWBD_list[present])
+        self.canvas_bottom.addtag_withtag('image', self.image_added)
+        # 棋子与位置标签绑定，方便“杀死”
+        self.canvas_bottom.addtag_withtag('position' + str(p1) + str(p2), self.image_added)
+
+    def create_sign(self, x1, x2, x3, x4):
+        # 删除上次的标记，重新创建标记
+        self.canvas_bottom.delete('image_added_sign')
+        self.image_added_sign = self.canvas_bottom.create_oval(x1, x2, x3, x4, width=3, outline='#3ae')
+        self.canvas_bottom.addtag_withtag('image', self.image_added_sign)
+        self.canvas_bottom.addtag_withtag('image_added_sign', self.image_added_sign)
+
+class Weiqi():
+    def __init__(self, chessboard_size=9):
+        # 模式，九路棋：9，十三路棋：13，十九路棋：19
+        self.mode_num = chessboard_size
+        self.window_size = 1.8
+        self.dd = 360 * self.window_size / (self.mode_num - 1)  # 棋盘每格的边长
+        # 棋盘的相对矫正比例
+        self.p = 1 if self.mode_num == 9 else (2 / 3 if self.mode_num == 13 else 4 / 9)
+        self.window = Window(self, self.window_size, self.dd, self.p, chessboard_size)
+        # 棋盘阵列,超过边界：-1，无子：0，黑棋：1，白棋：2
+        self.chessboard = [[0 for i in range(self.mode_num + 2)] for i in range(self.mode_num + 2)]
+        for m in range(self.mode_num + 2):
+            for n in range(self.mode_num + 2):
+                if m == 0 or n == 0 or m == self.mode_num + 1 or n == self.mode_num + 1:
+                    self.chessboard[m][n] = -1
+        # 拷贝三份棋盘“快照”，悔棋和判断“打劫”时需要作参考
+        self.last_3_chessboard = copy.deepcopy(self.chessboard)
+        self.last_2_chessboard = copy.deepcopy(self.chessboard)
+        self.last_1_chessboard = copy.deepcopy(self.chessboard)
+
+        # 当前轮到的玩家，黑：0，白：1，执黑先行
+        self.present = 0
+        # 初始停止，点击“开始游戏”运行游戏
+        self.stop = True
+        # 悔棋次数，次数大于0才可悔棋，初始置0（初始不能悔棋），悔棋后置0，下棋或弃手时恢复为1，以禁止连续悔棋
+        self.regretchance = 0
+
+
+    # 开始游戏函数
+    def start(self):
+        # 图标处理
+        self.window.start(self.present)
         # 开始标志，解除stop
         self.stop = None
 
@@ -140,35 +212,25 @@ class Weiqi(Tk):
         if not self.regretchance == 1:
             self.regretchance += 1
         else:
-            self.regretButton['state'] = NORMAL
+            self.window.regretButton['state'] = NORMAL
         # 拷贝棋盘状态，记录前三次棋局
         self.last_3_chessboard = copy.deepcopy(self.last_2_chessboard)
         self.last_2_chessboard = copy.deepcopy(self.last_1_chessboard)
         self.last_1_chessboard = copy.deepcopy(self.chessboard)
-        self.canvas_bottom.delete('image_added_sign')
+        self.window.canvas_bottom.delete('image_added_sign')
         # 轮到下一玩家
-        if self.present == 0:
-            self.create_pW()
-            self.del_pB()
-            self.present = 1
-        else:
-            self.create_pB()
-            self.del_pW()
-            self.present = 0
+        self.window.player_change(self.present)
+        self.present = 1 - self.present
 
     # 悔棋函数，可悔棋一回合，下两回合不可悔棋
     def regret(self):
         # 判定是否可以悔棋，以前第三手棋局复原棋盘
         if self.regretchance == 1:
             self.regretchance = 0
-            self.regretButton['state'] = DISABLED
+            self.window.regretButton['state'] = DISABLED
             list_of_b = []
             list_of_w = []
-            self.canvas_bottom.delete('image')
-            if self.present == 0:
-                self.create_pB()
-            else:
-                self.create_pW()
+            self.window.regret(self.present)  # 修改UI
             for m in range(1, self.mode_num + 1):
                 for n in range(1, self.mode_num + 1):
                     self.chessboard[m][n] = 0
@@ -190,33 +252,15 @@ class Weiqi(Tk):
     def reload(self):
         if self.stop == 1:
             self.stop = 0
-        self.canvas_bottom.delete('image')
         self.regretchance = 0
         self.present = 0
-        self.create_pB()
+        self. window.reload()
         for m in range(1, self.mode_num + 1):
             for n in range(1, self.mode_num + 1):
                 self.chessboard[m][n] = 0
                 self.last_3_chessboard[m][n] = 0
                 self.last_2_chessboard[m][n] = 0
                 self.last_1_chessboard[m][n] = 0
-
-    # 显示鼠标移动下棋子的移动
-    def shadow(self, event):
-        if not self.stop:
-            # 找到最近格点，在当前位置靠近的格点出显示棋子图片，并删除上一位置的棋子图片
-            if (20 * self.window_size < event.x < 380 * self.window_size) \
-                and (20 * self.window_size < event.y < 380 * self.window_size):
-                dx = (event.x - 20 * self.window_size) % self.dd
-                dy = (event.y - 20 * self.window_size) % self.dd
-                self.cross = self.canvas_bottom.create_image(
-                    event.x - dx + round(dx / self.dd) * self.dd + 22 * self.p,
-                    event.y - dy + round(dy / self.dd) * self.dd - 27 * self.p,
-                    image=self.photoWBU_list[self.present])
-                self.canvas_bottom.addtag_withtag('image', self.cross)
-                if self.cross_last != None:
-                    self.canvas_bottom.delete(self.cross_last)
-                self.cross_last = self.cross
 
     # 落子，并驱动玩家的轮流下棋行为
     def getDown(self, event):
@@ -232,13 +276,11 @@ class Weiqi(Tk):
                 if self.chessboard[y][x] == 0:
                     # 未被占据，则尝试占据，获得占据后能杀死的棋子列表
                     self.chessboard[y][x] = self.present + 1
-                    self.image_added = self.canvas_bottom.create_image(
+                    self.window.add_image(
                         event.x - dx + round(dx / self.dd) * self.dd + 4 * self.p,
                         event.y - dy + round(dy / self.dd) * self.dd - 5 * self.p,
-                        image=self.photoWBD_list[self.present])
-                    self.canvas_bottom.addtag_withtag('image', self.image_added)
-                    # 棋子与位置标签绑定，方便“杀死”
-                    self.canvas_bottom.addtag_withtag('position' + str(x) + str(y), self.image_added)
+                        x, y, self.present
+                    )
                     deadlist = self.get_deadlist(x, y)
                     self.kill(deadlist)
                     # 判断是否重复棋局
@@ -249,47 +291,39 @@ class Weiqi(Tk):
                             if not self.regretchance == 1:
                                 self.regretchance += 1
                             else:
-                                self.regretButton['state'] = NORMAL
+                                self.window.regretButton['state'] = NORMAL
                             self.last_3_chessboard = copy.deepcopy(self.last_2_chessboard)
                             self.last_2_chessboard = copy.deepcopy(self.last_1_chessboard)
                             self.last_1_chessboard = copy.deepcopy(self.chessboard)
                             # 删除上次的标记，重新创建标记
-                            self.canvas_bottom.delete('image_added_sign')
-                            self.image_added_sign = self.canvas_bottom.create_oval(
+                            self.window.create_sign(
                                 event.x - dx + round(dx / self.dd) * self.dd + 0.5 * self.dd,
                                 event.y - dy + round(dy / self.dd) * self.dd + 0.5 * self.dd,
                                 event.x - dx + round(dx / self.dd) * self.dd - 0.5 * self.dd,
-                                event.y - dy + round(dy / self.dd) * self.dd - 0.5 * self.dd, width=3,
-                                outline='#3ae')
-                            self.canvas_bottom.addtag_withtag('image', self.image_added_sign)
-                            self.canvas_bottom.addtag_withtag('image_added_sign', self.image_added_sign)
-                            if self.present == 0:
-                                self.create_pW()
-                                self.del_pB()
-                                self.present = 1
-                            else:
-                                self.create_pB()
-                                self.del_pW()
-                                self.present = 0
+                                event.y - dy + round(dy / self.dd) * self.dd - 0.5 * self.dd
+                            )
+                            # 轮到下一玩家
+                            self.window.player_change(self.present)
+                            self.present = 1 - self.present
                         else:
                             # 不属于杀死对方或有气，则判断为无气，警告并弹出警告框
                             self.chessboard[y][x] = 0
-                            self.canvas_bottom.delete('position' + str(x) + str(y))
-                            self.bell()
-                            self.showwarningbox('无气', "你被包围了！")
+                            self.window.canvas_bottom.delete('position' + str(x) + str(y))
+                            self.window.bell()
+                            self.window.showwarningbox('无气', "你被包围了！")
                     else:
                         # 重复棋局，警告打劫
                         self.chessboard[y][x] = 0
-                        self.canvas_bottom.delete('position' + str(x) + str(y))
+                        self.window.canvas_bottom.delete('position' + str(x) + str(y))
                         self.recover(deadlist, (1 if self.present == 0 else 0))
-                        self.bell()
-                        self.showwarningbox("打劫", "此路不通！")
+                        self.window.bell()
+                        self.window.showwarningbox("打劫", "此路不通！")
                 else:
                     # 覆盖，声音警告
-                    self.bell()
+                    self.window.bell()
             else:
                 # 超出边界，声音警告
-                self.bell()
+                self.window.bell()
 
     # 判断棋子（种类为yourChessman，位置为yourPosition）是否无气（死亡），有气则返回False，无气则返回无气棋子的列表
     # 本函数是游戏规则的关键，初始deadlist只包含了自己的位置，每次执行时，函数尝试寻找yourPosition周围有没有空的位置，有则结束，返回False代表有气；
@@ -338,10 +372,6 @@ class Weiqi(Tk):
                 deadList += copy.deepcopy(midvar)
         return deadList
 
-    # 警告消息框，接受标题和警告信息
-    def showwarningbox(self, title, message):
-        self.canvas_bottom.delete(self.cross)
-        tkinter.messagebox.showwarning(title, message)
 
     # 落子后，依次判断四周是否有棋子被杀死，并返回死棋位置列表
     def get_deadlist(self, x, y):
@@ -362,37 +392,36 @@ class Weiqi(Tk):
         if len(list_to_recover) > 0:
             for i in range(len(list_to_recover)):
                 self.chessboard[list_to_recover[i][1]][list_to_recover[i][0]] = b_or_w + 1
-                self.image_added = self.canvas_bottom.create_image(
+                self.window.add_image(
                     20 * self.window_size + (list_to_recover[i][0] - 1) * self.dd + 4 * self.p,
                     20 * self.window_size + (list_to_recover[i][1] - 1) * self.dd - 5 * self.p,
-                    image=self.photoWBD_list[b_or_w])
-                self.canvas_bottom.addtag_withtag('image', self.image_added)
-                self.canvas_bottom.addtag_withtag(
-                    'position' + str(list_to_recover[i][0]) + str(list_to_recover[i][1]), self.image_added)
+                    list_to_recover[i][0], list_to_recover[i][1], b_or_w
+                )
+
 
     # 杀死位置列表killList中的棋子，即删除图片，位置值置0
     def kill(self, killList):
         if len(killList) > 0:
             for i in range(len(killList)):
                 self.chessboard[killList[i][1]][killList[i][0]] = 0
-                self.canvas_bottom.delete('position' + str(killList[i][0]) + str(killList[i][1]))
+                self.window.canvas_bottom.delete('position' + str(killList[i][0]) + str(killList[i][1]))
 
     # 键盘快捷键退出游戏
     def keyboardQuit(self, event):
-        self.quit()
+        self.window.quit()
 
     # 以下两个函数修改全局变量值，newApp使主函数循环，以建立不同参数的对象
     def newGame1(self):
         global mode_num, newApp
         mode_num = (13 if self.mode_num == 9 else 9)
         newApp = True
-        self.quit()
+        self.window.quit()
 
     def newGame2(self):
         global mode_num, newApp
         mode_num = (13 if self.mode_num == 19 else 19)
         newApp = True
-        self.quit()
+        self.window.quit()
 
 
 # 声明全局变量，用于新建Application对象时切换成不同模式的游戏
@@ -404,9 +433,9 @@ if __name__ == '__main__':
     while True:
         newApp = False
         app = Weiqi(mode_num)
-        app.title('围棋')
-        app.mainloop()
+        app.window.title('围棋')
+        app.window.mainloop()
         if newApp:
-            app.destroy()
+            app.window.destroy()
         else:
             break
